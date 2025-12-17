4e804f63fbd114b9099c0908fde62c5dcc4780b5
Shok <duchamp2619@mail.ru>
2025-12-17 10:21:33 +0300

    FifTh commit

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -10,7 +10,7 @@ export default () => ({
   },
   
   chatwoot: {
-    apiBaseUrl: process.env.API_BASE_URL || 'https://guiai-test.ru',
+    apiBaseUrl: process.env.API_BASE_URL || 'https://guiai-test.ru/api/v1/accounts/1',
     apiToken: process.env.API_TOKEN,
     accessToken: process.env.ACCESS_TOKEN,
     inboxId: parseInt(process.env.INBOX_ID, 10),
